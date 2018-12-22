7e7f4baa4ddb889ec0e87603fc8b76072a75ba7a
Allister <ageebernal@aol.com>
2018-12-22 00:11:03 -0800

    add logos and edit functions

--- public/functions/results.js
+++ public/functions/results.js
@@ -1,25 +1,67 @@
-console.log(localStorage.getItem('query'));
-const results = document.querySelector('#listResults');
+//Declare HTML elements from results.hbs
+let results = document.querySelector('#listResults'),
+      resultsMessage = document.querySelector('#resultsMessage'),
+      searchBox = document.querySelector('#searchName');
 
-function getResults(e) {
+
+
+function searchResults(e) {
+  //Prevent a refresh of the page.
   e.preventDefault();
-  let query = localStorage.getItem('query');
-  let queryURI = encodeURIComponent(query);
-  let querycodeUrl = `https://api.trade.gov/consolidated_screening_list/search?api_key=lVRffURh533foYGOFnvH6gnA&q=${queryURI}`
-
+  //set the query to either the home page's or the result's
+  let query = searchBox.querySelector('input').value || localStorage.getItem('query');
+      queryURI = encodeURIComponent(query),
+      querycodeUrl = `https://api.trade.gov/consolidated_screening_list/search?api_key=lVRffURh533foYGOFnvH6gnA&q=${queryURI}`;
 
   axios.get(querycodeUrl).then((res) => {
-    console.log(res.data.results);
-    results.innerHTML = res.data.results.map((sdn) => {
-      return `
-        <tr>
-          <td>${sdn.name}</td>
-          <td>${sdn.programs}</td>
-          <td>${query}</td>
-        </tr>
-      `
-    }).join('')
+    //Store response data into local storage.
+    localStorage.setItem('results', JSON.stringify(res.data.results));
+    //Display results message based on response.
+    if (res.data.results.length == 0) {
+      resultsMessage.innerHTML = `No results for <b><i>${query}</i></b>`;
+    }else{
+      resultsMessage.innerHTML = `Your results for <b><i>${query}</i></b>`;
+
+    }
+
+    //Display list of results in table.
+    displayResults(res, query);
+
   }).catch(err => console.log(err));
 }
 
-window.onload = getResults;
+function displayResults(res, query) {
+
+  results.innerHTML = res.data.results.map((sdn) => {
+    return `
+      <tr onclick="displayProfile('${sdn.id}')" id='${sdn.id}'>
+        <td>${sdn.name}</td>
+        <td>${sdn.type || 'N/A'}</td>
+        <td>${sdn.source}</td>
+        <td><i>${query}</i></td>
+      </tr>
+      `
+  }).join('');
+}
+
+function displayProfile(id) {
+  let sdn = JSON.parse(localStorage.getItem('results')).find(sdn => sdn.id == id),
+      details;
+  if ('type' in sdn) {
+    details = `
+      <img src='logos/${sdn.type}-icon.png' alt='${sdn.type} logo'>
+      <li id='type'><b>Type:</b> ${sdn.type}</li>
+      <li id='fullName'><b>Full Name:</b> ${sdn.name};
+      <li id='program'><b>Program:</b> ${sdn.source}</li>
+    `;
+  }else{
+    details = `
+      <li id='fullName'><b>Full Name:</b> ${sdn.name}
+      <li id='program'><b>Program:</b> ${sdn.source}</li>
+    `;
+  }
+  document.querySelector('#details').innerHTML = details;
+}
+
+window.onload = searchResults;
+searchBox.addEventListener('submit', searchResults);
