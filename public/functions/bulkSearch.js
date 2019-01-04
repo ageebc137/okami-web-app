4e3d6ce972d11a6cc4e190ab56969290dc274608
Allister <ageebernal@aol.com>
2019-01-03 21:42:58 -0800

    added files

---
+++ public/functions/bulkSearch.js
@@ -0,0 +1,36 @@
+let btns = document.querySelectorAll("button"),
+    querySection = document.querySelector("#inputSection");
+
+function toggle(e) {
+  if (this.id === 'add-button') {
+    addQuery();
+  }else if (this.id === 'remove-button') {
+    removeQuery(e);
+  }else {
+    searchQueries(e);
+  }
+}
+
+function addQuery() {
+  if (querySection.childElementCount >= 10) {return;}
+  let input = document.createElement('li');
+  input.innerHTML = `<input type="textarea" placeholder=' Search entity or individual here' id='searchInput' /required>`;
+  querySection.appendChild(input);
+}
+
+function removeQuery(e) {
+  e.preventDefault();
+  if (querySection.childElementCount <= 2) {return;}
+  let queries = document.querySelectorAll(`ul[id='inputSection'] > li`);
+  querySection.removeChild(queries[queries.length - 1]);
+}
+
+function searchQueries(e) {
+  let queries = document.querySelectorAll(`ul[id='inputSection'] > li`);
+  let array = [];
+  queries.forEach(query => array.push(query.firstChild.value));
+  localStorage.setItem('bulkQuery', array);
+  window.location.href = '/results';
+}
+
+btns.forEach(btn => btn.addEventListener('click', toggle));
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const yargs = require('yargs');
+
+var app = express();
+
+let port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+
+// const argv = yargs
+//     .options({
+//       q: {
+//           demand: true,
+//           alias: 'query',
+//           describe: 'Query to fetch SDN entity or individual',
+//           string: true
+//       }
+//     })
+//       .help()
+//       .alias('help', 'h')
+//       .argv;
+//
+// console.log('You searched:', argv.query);
+//
+// let queryURI = encodeURIComponent(argv.query);
+// let querycodeUrl = `https://api.trade.gov/consolidated_screening_list/search?api_key=lVRffURh533foYGOFnvH6gnA&q=${queryURI}`
+//
+// axios.get(querycodeUrl).then((response) => {
+//   console.log(JSON.stringify(response.data.results, undefined, 2));
+// }).catch((e) => {
+//   console.log(e.message);
+// })
+
+app.get(('/'), (req, res) => {
+  res.render('home.hbs');
+});
+
+app.get(('/about'), (req, res) => {
+  res.render('about.hbs');
+});
+
+app.get(('/contacts'), (req, res) => {
+  res.render('contacts.hbs');
+});
+
+app.get(('/results'), (req, res) => {
+  res.render('results.hbs');
+});
+
+app.get(('/bulksearch'), (req, res) => {
+  res.render('bulk.hbs');
+})
+
+app.listen(port, () => {
+  console.log(`Server is on port ${port}`);
+});
