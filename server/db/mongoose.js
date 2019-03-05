fc57450b8bdc9a40c516a3673f1cd93a1f4e232b
Allister <ageebernal@aol.com>
2019-03-05 15:08:42 -0800

    mongoose.js file changed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then(() => {
-  console.log("You are connected to the database");
+  console.log("You are connected to the database", process.env.MONGODB_URI);
 })
   .catch((err) => console.log("No connection to database", err));
 
